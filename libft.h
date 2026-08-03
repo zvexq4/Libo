299314ae4a4f5c8dfa0428bd26b9b7fd8c21ec45
Hamza Ciftci <hciftci@k1m26s03.42istanbul.com.tr>
2026-08-03 18:11:48 +0300

    Karakter kontrol ve dönüşüm fonksiyonları tamamlandı

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hciftci <hciftci@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/03 13:51:03 by hciftci           #+#    #+#             */
+/*   Updated: 2026/08/03 17:59:43 by hciftci          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+
+#endif 
